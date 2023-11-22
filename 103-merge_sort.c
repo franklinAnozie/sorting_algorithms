af8be292041b1aeb1cb6294e06416a1ba352980d
franklinAnozie <cfanozie@gmail.com>
2023-11-22 16:44:50 +0100

    fixed issues with sorting a deck of cards

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,67 +1,85 @@
 #include "sort.h"
 
+/**
+ * merge_sort - sorts an array of integers in ascending order
+ * @array: the array to sort
+ * @size: the size of the array
+ */
 void merge_sort(int *array, size_t size)
 {
-    int size_c = (int)size;
-    int start = 0, end = size_c - 1;
+	int size_c = (int)size;
+	int start = 0, end = size_c - 1;
 
-    if (array == NULL)
-        exit(EXIT_FAILURE);
-    
-    merge_sort_rec(array, start, end);
+	if (array == NULL || size < 2)
+		return;
+
+	merge_sort_rec(array, start, end);
 }
 
+/**
+ * merge_sort_rec - recursively merges two sorted arrays
+ * @array: the array to merge
+ * @start: the start of the array
+ * @end: the end of the array
+*/
 void merge_sort_rec(int *array, int  start, int end)
 {
-    int mid;
+	int mid;
 
-    if (start != end)
-    {
-        mid = start + (end - start) / 2;
+	if (start != end)
+	{
+		mid = start + (end - start) / 2;
 
-        merge_sort_rec(array, start, mid);
-        merge_sort_rec(array, mid + 1, end);
+		merge_sort_rec(array, start, mid);
+		merge_sort_rec(array, mid + 1, end);
 
-        merge_arrays(array, start, mid, end);
-    }
+		merge_arrays(array, start, mid, end);
+	}
 }
 
+/**
+ * merge_arrays - merges two sorted arrays
+ * @array: the array to merge
+ * @start: the start of the first array
+ * @mid: the end of the first array
+ * @end: the end of the second array
+*/
 void merge_arrays(int *array, int start, int mid, int end)
 {
-    int i, j, k;
-    int *new_array;
+	int i, j, k;
+	int *new_array;
 
-    printf("Merging...\n");
+	printf("Merging...\n");
 
-    new_array = malloc(sizeof(int) * (end + 1));
-    if (new_array == NULL)
-        exit(EXIT_FAILURE);
-    i = start;
-    j = mid + 1;
-    k = start;
+	new_array = malloc(sizeof(int) * (end + 1));
+	if (new_array == NULL)
+		exit(EXIT_FAILURE);
+	i = start;
+	j = mid + 1;
+	k = start;
 
-    printf("[left]: ");
-    print_array(array + start, mid - start + 1);
-    printf("[right]: ");
-    print_array(array + mid + 1, end - mid);
-    while (i <= mid && j <= end)
-    {
-        if (array[i] <= array[j])
-            new_array[k++] = array[i++];
-        else
-            new_array[k++] = array[j++];
-    }
-    while (j <= end)
-        new_array[k++] = array[j++];
-    
-    while (i <= mid)
-        new_array[k++] = array[i++];
-        
-    for (i = start; i <= end; i++)
-        array[i] = new_array[i];
-    
-    printf("[Done]: ");
-    print_array(array + start, end - start + 1);
-    
-    free(new_array);
+	printf("[left]: ");
+	print_array(array + start, mid - start + 1);
+	printf("[right]: ");
+	print_array(array + mid + 1, end - mid);
+	while (i <= mid && j <= end)
+	{
+		if (array[i] <= array[j])
+			new_array[k++] = array[i++];
+		else
+			new_array[k++] = array[j++];
+	}
+	while (j <= end)
+		new_array[k++] = array[j++];
+
+	while (i <= mid)
+		new_array[k++] = array[i++];
+
+	for (i = start; i <= end; i++)
+		array[i] = new_array[i];
+
+	printf("[Done]: ");
+	print_array(array + start, end - start + 1);
+
+	free(new_array);
 }
--- deck.h
+++ deck.h
@@ -2,16 +2,21 @@
 #define _DECK_H_
 
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <stddef.h>
 
+/**
+ * enum kind_e - Kind of card_s - Playing card
+ * @SPADE: Spades
+ * @HEART: Hearts
+ * @CLUB: Clubs
+ * @DIAMOND: Diamonds
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -23,8 +28,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -36,9 +41,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
